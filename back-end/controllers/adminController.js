bf6d7096d208af72646be3c593778197233cbf20
Fachou01 <koumenjimohamed@gmail.com>
2022-08-04 19:39:49 +0100

    setting up admin controller

--- back-end/controllers/adminController.js
+++ back-end/controllers/adminController.js
@@ -1,17 +1,91 @@
-const getAdmins = (req, res) => {
-  res.send("het");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+const getAdmins = async (req, res) => {
+  try {
+    const admins = await prisma.admin.findMany({});
+    res.status(200).json(admins);
+  } catch (error) {
+    res.send("Error get admins");
+  }
 };
 
-const getAdminById = (req, res) => {
+const getAdminById = async (req, res) => {
   const id = req.params.id;
-  res.send(id);
+  try {
+    const admin = await prisma.admin.findFirst({
+      where: {
+        id: id,
+      },
+    });
+    res.status(200).json(admin);
+  } catch (error) {
+    res.send("Error get adminById");
+  }
 };
 
-const addAdmin = (req, res) => {};
+const addAdmin = async (req, res) => {
+  const { name, email, password } = req.body;
+  //find the admin with the given email
+  try {
+    const admin = await prisma.admin.findFirst({
+      where: {
+        email: email,
+      },
+    });
+    //if admin exist
+    if (admin) res.send("User already exist !");
+    else {
+      try {
+        //hash password
+        const newAdmin = await prisma.admin.create({
+          data: {
+            name,
+            email,
+            password,
+          },
+        });
+        res.status(201).json(newAdmin);
+      } catch (error) {
+        res.send("Error creating user");
+      }
+    }
+  } catch (error) {
+    res.send("Error");
+  }
 
-const updateAdminById = (req, res) => {};
+  //add to db
+};
 
-const deleteAdminById = (req, res) => {};
+const updateAdminById = async (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  try {
+    const updatedAdmin = await prisma.admin.update({
+      where: {
+        id: +id,
+      },
+      data: body,
+    });
+    res.status(200).json(updatedAdmin);
+  } catch (error) {
+    res.send("Error update adminById");
+  }
+};
+
+const deleteAdminById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const admin = await prisma.admin.delete({
+      where: {
+        id: +id,
+      },
+    });
+    res.status(200).json(admin);
+  } catch (error) {
+    res.send("Error delete adminById");
+  }
+};
 
 module.exports = {
   getAdmins,
--- back-end/index.js
+++ back-end/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const usersRoute = require("./routes/admin");
-
 const app = express();
 app.use(express.json());
 
